3b6e8da44d81a0577c382116ac248826fcb169a0
Noelia Camelia <noeliacamelia@gmail.com>
2025-03-07 16:52:05 +0100

    error pageAbout

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,13 +1,14 @@
-
-
 export const pageAbout = () => {
-    return (
-      <div>
-        <p>page About</p>
-      <p>Que es lo que involucra al club, lo de sin fronteras, las actividades de taegondo, venta de comida? </p>
+  return (
+    <div>
+      <p>page About</p>
+      <p>
+        Que es lo que involucra al club, lo de sin fronteras, las actividades de
+        taegondo, venta de comida?{" "}
+      </p>
       <p>Alquiler del club</p>
-      </div>
-    )
-  }
-  
-  export default pageAbout
+    </div>
+  );
+};
+
+export default pageAbout;
--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,16 +1,13 @@
-
-
 export const pageContant = () => {
-    return (
-      <div className="flex flex-col justify-center items-center h-screen">
-        En este sector se mostrará el contenido de la página de contacto:
-        que asociaciones hay dentro del club,
-        un mapa con la dirección del club, 
-        los telefonos a los que pueden llamar de cada una de las asociaciones para obtener más información,
-        el correo electrónico si es que tienen cada una de las asociaciones,
-        una foto del frente del club, etc.
-      </div>
-    )
-  }
-  
-  export default pageContant
+  return (
+    <div className="flex flex-col justify-center items-center h-screen">
+      En este sector se mostrará el contenido de la página de contacto: que
+      asociaciones hay dentro del club, un mapa con la dirección del club, los
+      telefonos a los que pueden llamar de cada una de las asociaciones para
+      obtener más información, el correo electrónico si es que tienen cada una
+      de las asociaciones, una foto del frente del club, etc.
+    </div>
+  );
+};
+
+export default pageContant;
